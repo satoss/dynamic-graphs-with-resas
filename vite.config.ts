3ea8f1dcac8b0b23511be481882487de294310da
shoei_sato <46258448+satoss@users.noreply.github.com>
2022-10-23 19:57:52 +0900

    set up

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  root: './src',
+  publicDir: '../public',
+  build: {
+    outDir: '../dist',
+  },
+  server: {
+    open: true,
+  },
+  plugins: [react()],
 })
